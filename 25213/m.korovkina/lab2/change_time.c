debb4455538dbb2bf8688ad7ceb5768d641b9b94
Marianna Korovkina <m.korovkina@g.nsu.ru>
2026-09-10 22:48:21 +0700

    lab2

---
+++ 25213/m.korovkina/lab2/change_time.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <time.h>
+
+extern char* tzname[];
+
+int main() {
+  int result = putenv("TZ=PST8PDT");
+  if (result != 0) {
+    perror("Failed to change TZ");
+    exit(1);
+  }
+  tzset();
+  time_t now;
+  struct tm* sp;
+  (void)time(&now);
+  printf("%s", ctime(&now));
+  sp = localtime(&now);
+  printf("%d/%d/%02d %d:%02d %s\n", sp->tm_mon + 1, sp->tm_mday, sp->tm_year,
+         sp->tm_hour, sp->tm_min, tzname[sp->tm_isdst]);
+  exit(0);
+}
